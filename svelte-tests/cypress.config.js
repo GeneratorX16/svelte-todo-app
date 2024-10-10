fc7824f6fbfeaef791742e780b308234e7b9e93b
GeneratorX16 <udeveer0321@gmail.com>
2024-10-10 15:06:59 +0530

    Updated mocha reporter

--- svelte-tests/cypress.config.js
+++ svelte-tests/cypress.config.js
@@ -5,7 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'http://localhost:8080',
+    reporter: 'mocha-junit-reporter', // Using Mocha JUnit Reporter
+    reporterOptions: {
+      mochaFile: 'cypress/results/test-output-[hash].xml' // Output path for JUnit report
+    },
   },
   compilerOptions: {
     allowJs: true,
